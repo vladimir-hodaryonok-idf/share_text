21e87788df4dde343012f37f23818141d5c7c023
vladimir <vladimir.hodaryonok>
2022-10-12 21:19:58 +0300

    0.0.4

--- android/src/main/kotlin/com/vladimir_/hodaryonok/share_text/ShareTextPlugin.kt
+++ android/src/main/kotlin/com/vladimir_/hodaryonok/share_text/ShareTextPlugin.kt
@@ -1,5 +1,6 @@
 package com.vladimir_.hodaryonok.share_text
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.annotation.NonNull
@@ -11,37 +12,38 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** ShareTextPlugin */
-class ShareTextPlugin: FlutterPlugin, MethodCallHandler {
+class ShareTextPlugin : FlutterPlugin, MethodCallHandler {
 
-  private lateinit var channel : MethodChannel
-  private lateinit var context: Context
+    private lateinit var channel: MethodChannel
+    private lateinit var context: Context
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "share_text")
-    channel.setMethodCallHandler(this)
-    context = flutterPluginBinding.getApplicationContext()
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "share_text") {
-      val message = call.arguments  as String
-      shareText(message)
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "share_text")
+        channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
-
-  private fun shareText(message: String) {
-    val sendIntent: Intent = Intent().apply {
-      action = Intent.ACTION_SEND
-      putExtra(Intent.EXTRA_TEXT, message)
-      type = "text/plain"
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "share_text") {
+            val message = call.arguments as String
+            shareText(message)
+        } else {
+            result.notImplemented()
+        }
     }
-    val sharingIntent = Intent.createChooser(sendIntent, "Movie Sharing")
-    context.startActivity(sharingIntent)
-  }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    private fun shareText(message: String) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, message)
+            type = "text/plain"
+        }
+        val sharingIntent = Intent.createChooser(sendIntent, "Movie Sharing")
+        sharingIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        context.startActivity(sharingIntent)
+    }
 }
